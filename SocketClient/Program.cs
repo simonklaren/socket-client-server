b48b3d43b371d0ede1367db29ca479493866a40e
Simon Klaren <Simon@klaren.nl>
2026-05-12 17:00:40 +0200

    add client that connects to server and sends console inputs to server and prints response

--- SocketClient/Program.cs
+++ SocketClient/Program.cs
@@ -1,1 +1,45 @@
-﻿Console.Write("Socket server starting...");
+﻿using System.Net;
+using System.Net.Sockets;
+
+
+Console.Write("Socket server starting...");
+
+// connect to localhost (socketServer)
+const string host = "localhost";
+const int port = 5000;
+
+var client = new TcpClient();
+
+client.Connect(host, port);
+
+Console.WriteLine("Client: connected to server");
+
+// get stream and read it 
+var stream = client.GetStream();
+var reader = new StreamReader(stream);
+
+// read server message
+var serverMessage = reader.ReadLine();
+
+Console.WriteLine("Server: " + serverMessage);
+
+// add writer so we can send a message back to server
+var writer = new StreamWriter(stream) { AutoFlush = true };
+
+while (true)
+{
+    // send input from console to writer
+    Console.Write("Client: ");
+    var input = Console.ReadLine();
+    writer.WriteLine(input);
+
+    var serverResponse = reader.ReadLine();
+    Console.WriteLine("Server: " + serverResponse);
+
+    if (input == "stop")
+    {
+        break;
+    }
+}
+
+client.Close(); // finally close client
